93dd593c1a37761f8b140904e3c64e39f8624606
babyazalea <usun12@hotmail.com>
2021-12-15 09:57:26 +0900

    setter injection & field injection

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -1,12 +1,38 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class TennisCoach implements Coach{
+public class TennisCoach implements Coach {
+
+    @Autowired
+    private FortuneService fortuneService;
+
+    // define a default constructor
+    public TennisCoach() {
+        System.out.println(">> TennisCoach: inside default constructor");
+    }
+
+    // define a setter method
+    // @Autowired
+    // public void doSomeCrazyStuff(FortuneService theFortuneService) {
+    //     System.out.println(">> TennisCoach: inside default doSomeCrazyStuff() method");
+    //     fortuneService = theFortuneService;
+    // }
+
+    // @Autowired
+    // public TennisCoach(FortuneService theFortuneService) {
+    //     fortuneService = theFortuneService;
+    // }
 
     @Override
     public String getDailyWorkout() {
         return "Practice your backhand volley";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return fortuneService.getFortune();
     }
 }
