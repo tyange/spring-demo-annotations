ed492a38d7e9d34ccd4a0b7525d0c8df35357cf6
babyazalea <usun12@hotmail.com>
2021-12-17 18:15:00 +0900

    added logger

---
+++ src/com/luv2code/springdemo/JavaConfigDemoApp.java
@@ -0,0 +1,25 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class JavaConfigDemoApp {
+    public static void main(String[] args) {
+        // read spring config java class
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
+
+        // get the bean from spring container
+        // Component Annotation을 통해 특정한 name을 정의하지 않았을 때, getBean method의 name 파라미터에 해당 Class-name의
+        // 맨 앞 글자만 소문자로 바꾼 이름을 사용할 수 있다. (default로 적용됌)
+        // ex) class TennisCoach => name : "tennisCoach"
+        Coach theCoach = context.getBean("tennisCoach", Coach.class);
+
+        // call a method on the bean
+        System.out.println(theCoach.getDailyWorkout());
+
+        // call a method on the bean
+        System.out.println(theCoach.getDailyFortune());
+
+        // close the context
+        context.close();
+    }
+}
--- src/com/luv2code/springdemo/SportConfig.java
+++ src/com/luv2code/springdemo/SportConfig.java
@@ -1,19 +1,11 @@
 package com.luv2code.springdemo;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+
 
 @Configuration
-@PropertySource("classpath:mylogger.properties")
+@ComponentScan("com.luv2code.springdemo")
 public class SportConfig {
-    @Bean
-    public MyLoggerConfig myLoggerConfig(@Value("${root.logger.level}") String rootLoggerLevel,
-                                         @Value("${printed.logger.level}") String printedLoggerLevel) {
 
-        MyLoggerConfig myLoggerConfig = new MyLoggerConfig(rootLoggerLevel, printedLoggerLevel);
-
-        return myLoggerConfig;
-    }
 }
